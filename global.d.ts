3e2f17063a23506671589447ebbacfd57ff3d004
Andrew Kazakov <tosk-nn@yandex.ru>
2024-02-02 14:41:20 +0300

    Add missing `ts-reset` imports to glodal.d.ts

--- global.d.ts
+++ global.d.ts
@@ -6,12 +6,14 @@
 
 // import '@total-typescript/ts-reset/array-includes';
 // import '@total-typescript/ts-reset/array-index-of';
-// import '@total-typescript/ts-reset/dom';
 // import '@total-typescript/ts-reset/fetch';
+import '@total-typescript/ts-reset/filter-boolean'; // or: (see ^)
 import '@total-typescript/ts-reset/dist/filter-boolean';
 // import '@total-typescript/ts-reset/is-array';
 // import '@total-typescript/ts-reset/json-parse';
 // import '@total-typescript/ts-reset/map-has';
+// import '@total-typescript/ts-reset/storage';
+// import '@total-typescript/ts-reset/set-has';
 
 // import '@total-typescript/ts-reset';
 
